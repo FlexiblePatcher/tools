3c8f8ee2155704998e2dd79f8bcc1518b7c782d5
asasas9500 <58693270+asasas9500@users.noreply.github.com>
2024-07-07 18:50:20 -0300

    Fix line endings

--- rename.py
+++ rename.py
@@ -7,10 +7,10 @@ def main():
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument('path', type=pathlib.Path)
     args = parser.parse_args()
-    with args.path.open(newline='\r\n') as source:
+    with args.path.open(newline='') as source:
         contents = source.read()
     contents = re.sub(r'\bPlugin_FLEP\b', 'Plugin_FlexiblePatcher', contents).replace('https://raw.githubusercontent.com/asasas9500/flep/master/downloads/Manual_FLEP.pdf', 'https://raw.githubusercontent.com/FlexiblePatcher/FlexiblePatcher/master/downloads/Manual_FlexiblePatcher.pdf')
-    with args.path.open('w', newline='\r\n') as destination:
+    with args.path.open('w', newline='') as destination:
         destination.write(contents)
 
 
