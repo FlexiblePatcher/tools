162574119381fac4801b780a1b7f98a1c316c423
asasas9500 <58693270+asasas9500@users.noreply.github.com>
2024-10-27 11:50:27 -0200

    Improve renaming

--- rename.py
+++ rename.py
@@ -5,13 +5,14 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument('path', type=pathlib.Path)
+    parser.add_argument('path', nargs='+', type=pathlib.Path)
     args = parser.parse_args()
-    with args.path.open(newline='') as source:
-        contents = source.read()
-    contents = re.sub(r'\bPlugin_FLEP\b', 'Plugin_FlexiblePatcher', contents)
-    with args.path.open('w', newline='') as destination:
-        destination.write(contents)
+    for current_path in args.path:
+        with current_path.open(newline='') as source:
+            contents = source.read()
+        contents = re.sub(r'\bPlugin_FLEP\b', 'Plugin_FlexiblePatcher', contents)
+        with current_path.open('w', newline='') as destination:
+            destination.write(contents)
 
 
 if __name__ == '__main__':
