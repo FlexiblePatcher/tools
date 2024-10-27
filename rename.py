2423208b98a67a4ca730301b0a4ae8bb6885fbf1
asasas9500 <58693270+asasas9500@users.noreply.github.com>
2024-10-27 00:27:17 -0200

    Remove manual link replacement

--- rename.py
+++ rename.py
@@ -9,7 +9,7 @@ def main():
     args = parser.parse_args()
     with args.path.open(newline='') as source:
         contents = source.read()
-    contents = re.sub(r'\bPlugin_FLEP\b', 'Plugin_FlexiblePatcher', contents).replace('https://raw.githubusercontent.com/asasas9500/flep/master/downloads/Manual_FLEP.pdf', 'https://raw.githubusercontent.com/FlexiblePatcher/FlexiblePatcher/master/downloads/Manual_FlexiblePatcher.pdf')
+    contents = re.sub(r'\bPlugin_FLEP\b', 'Plugin_FlexiblePatcher', contents)
     with args.path.open('w', newline='') as destination:
         destination.write(contents)
 
